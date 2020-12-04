b14404fd6458154b4875ea60a7e49a8385ed3450
Jess Tucker <tuckerjess92@gmail.com>
2020-12-04 14:37:30 -0600

    Create Spring Boot REST project

---
+++ Giftr/src/main/java/com/skilldistillery/giftr/GiftrApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.giftr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftrApplication.class, args);
+	}
+
+}
