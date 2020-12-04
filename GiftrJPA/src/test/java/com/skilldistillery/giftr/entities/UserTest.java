ae31f8120184bb6a9447d408823dbcfa12d71719
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-12-04 14:57:43 -0700

    Stub User entity and unit test

--- GiftrJPA/src/test/java/com/skilldistillery/giftr/entities/UserTest.java
+++ GiftrJPA/src/test/java/com/skilldistillery/giftr/entities/UserTest.java
@@ -1,5 +1,49 @@
 package com.skilldistillery.giftr.entities;
 
-public class UserTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private User user;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("giftrPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		user = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(user);
+		assertEquals("11", user.getUsername());
+
+	}
 }
