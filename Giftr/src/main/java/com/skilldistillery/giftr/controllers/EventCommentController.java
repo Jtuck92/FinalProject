3a64fb236acbbd327343f02a449448f92b660410
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-12-05 13:33:35 -0700

    EventComment Controller and Rest Points complete and tested

--- Giftr/src/main/java/com/skilldistillery/giftr/controllers/EventCommentController.java
+++ Giftr/src/main/java/com/skilldistillery/giftr/controllers/EventCommentController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.giftr.controllers;
 
+import java.security.Principal;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,27 +29,96 @@ public class EventCommentController {
 	
 	private String username = "11";
 	
-	  @GetMapping("eventComments")
-	  public Set<EventComment> index(String username){
-	    return eventCommentSvc.index(username);
-	  }
-	  
-	  @GetMapping("eventComments/{id}")
-	  public EventComment show(@PathVariable int id, HttpServletResponse response){
-		
-		try {
-			EventComment eventComment = eventCommentSvc.show(username ,id);
-			response.setStatus(200);
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.setStatus(404);
+	
+//	************ SECURITY API REST POINTS ************************
+//	
+//	  @GetMapping("eventComments")
+//	  public Set<EventComment> index(Principal p){
+//	    return eventCommentSvc.index(p.getName());
+//	  }
+//	  
+//	  @GetMapping("eventComments/{id}")
+//	  public EventComment show(@PathVariable int id, HttpServletResponse response, Principal p){
+//		
+//		try {
+//			EventComment eventComment = eventCommentSvc.show(p.getName() ,id);
+//			response.setStatus(200);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			response.setStatus(404);
+//		}
+//			  
+//		  
+//		return eventCommentSvc.show(p.getName(),id);
+//		  }
+//	  
+//	  
+//		@PostMapping("eventComments")
+//		public EventComment addEventComment(@RequestBody EventComment userParam, HttpServletRequest request, HttpServletResponse response, Principal p){
+//			
+//			try {
+//				userParam = eventCommentSvc.create(p.getName(), userParam);
+//				response.setStatus(201);
+//				response.setHeader("Location", "api/eventComments/" + userParam.getId());
+//			} catch (Exception e) {
+//				response.setStatus(400);
+//			}
+//		  return userParam;
+//		}
+//	
+//		@PutMapping("eventComments/{id}")
+//		public EventComment updateEventComment(@PathVariable int id, @RequestBody EventComment userParam, HttpServletRequest request, HttpServletResponse response, Principal p){
+//			System.err.println(userParam);
+//			
+//			
+//			try {
+//				userParam = eventCommentSvc.update(p.getName(), id, userParam);
+//				response.setStatus(201);
+//				response.setHeader("Location", "api/eventComments/" + userParam.getId());
+//			} catch (Exception e) {
+//				response.setStatus(400);
+//			}
+//			System.err.println(userParam);
+//			return userParam;
+//		}
+//		@DeleteMapping("eventComments/{id}")
+//		public void deleteEventComment(@PathVariable int id, HttpServletRequest request, HttpServletResponse response, Principal p) {
+//			try {
+//				boolean delete = eventCommentSvc.destroy(p.getName(), id);
+//				if(delete) {
+//				response.setStatus(204);}
+//				else {
+//				  response.setStatus(404);
+//				}
+//				response.setHeader("Location", "api/eventComments/");
+//			} catch (Exception e) {
+//				response.setStatus(400);
+//			}
+//			
+//		}
+//	
+//	************ TEST API REST POINTS ************************
+		@GetMapping("eventComments")
+		public Set<EventComment> index(){
+			return eventCommentSvc.index(username);
 		}
-			  
-		  
-		return eventCommentSvc.show(username,id);
-		  }
-	  
-	  
+		
+		@GetMapping("eventComments/{id}")
+		public EventComment show(@PathVariable int id, HttpServletResponse response){
+			
+			try {
+				EventComment eventComment = eventCommentSvc.show(username ,id);
+				response.setStatus(200);
+			} catch (Exception e) {
+				e.printStackTrace();
+				response.setStatus(404);
+			}
+			
+			
+			return eventCommentSvc.show(username,id);
+		}
+		
+		
 		@PostMapping("eventComments")
 		public EventComment addEventComment(@RequestBody EventComment userParam, HttpServletRequest request, HttpServletResponse response){
 			
@@ -59,9 +129,9 @@ public class EventCommentController {
 			} catch (Exception e) {
 				response.setStatus(400);
 			}
-		  return userParam;
+			return userParam;
 		}
-	
+		
 		@PutMapping("eventComments/{id}")
 		public EventComment updateEventComment(@PathVariable int id, @RequestBody EventComment userParam, HttpServletRequest request, HttpServletResponse response){
 			System.err.println(userParam);
@@ -82,9 +152,9 @@ public class EventCommentController {
 			try {
 				boolean delete = eventCommentSvc.destroy(username, id);
 				if(delete) {
-				response.setStatus(204);}
+					response.setStatus(204);}
 				else {
-				  response.setStatus(404);
+					response.setStatus(404);
 				}
 				response.setHeader("Location", "api/eventComments/");
 			} catch (Exception e) {
--- Giftr/src/main/java/com/skilldistillery/giftr/services/EventCommentServiceImpl.java
+++ Giftr/src/main/java/com/skilldistillery/giftr/services/EventCommentServiceImpl.java
@@ -1,11 +1,15 @@
 package com.skilldistillery.giftr.services;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.giftr.entities.EventComment;
+import com.skilldistillery.giftr.entities.User;
 import com.skilldistillery.giftr.repositories.EventCommentRepository;
 import com.skilldistillery.giftr.repositories.UserRepository;
 
@@ -19,32 +23,89 @@ public class EventCommentServiceImpl implements EventCommentService {
 
 	@Override
 	public Set<EventComment> index(String username) {
-		// TODO Auto-generated method stub
+		User user = uRepo.findByUsername(username);
+		if(user != null) {
+			Set<EventComment> activeEventComments = new HashSet<EventComment>();
+			List<EventComment> all = eCRepo.findAll();
+			for (EventComment eventComment : all) {
+				activeEventComments.add(eventComment);
+				return activeEventComments;
+			}	
+			}
 		return null;
 	}
 
 	@Override
 	public EventComment show(String username, int id) {
-		// TODO Auto-generated method stub
+		User user = uRepo.findByUsername(username);
+		if(user != null) {
+			
+		Optional<EventComment> ePOpt = eCRepo.findById(id);
+		EventComment eComm = null;
+		if(ePOpt.isPresent()) {
+			eComm = ePOpt.get();
+			if(eComm.isEnabled()) {
+			if (user.getEventComments().contains(eComm)) {
+					return eComm;					
+			}
+				}
+			}			
+		}
+		return null;
+	}
+	
+
+	@Override
+	public EventComment create(String username, EventComment eventComment) {
+		User user = uRepo.findByUsername(username);
+		if (user != null) {
+			eCRepo.saveAndFlush(eventComment);
+			user.getEventComments().add(eventComment);
+			uRepo.save(user);
+			return eventComment;
+		}		
 		return null;
 	}
 
 	@Override
-	public EventComment create(String username, EventComment event) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public EventComment update(String username, int id, EventComment event) {
-		// TODO Auto-generated method stub
+	public EventComment update(String username, int id, EventComment eventComment) {
+		User user = uRepo.findByUsername(username);
+		if (user != null) {
+			Optional<EventComment> ePOpt = eCRepo.findById(id);
+			EventComment eComm = null;
+			if(ePOpt.isPresent()) {
+				eComm = ePOpt.get();
+				if(eComm.isEnabled()) {
+					if(eComm.getComment() != null) {eComm.setComment(eventComment.getComment());}
+					if(eComm.getPost() != null) {eComm.setPost(eventComment.getPost());}
+					if(eComm.getUser() != null) {eComm.setUser(eventComment.getUser());}
+					eCRepo.saveAndFlush(eComm);
+					uRepo.saveAndFlush(user);
+					}
+			
+				}
+			}
 		return null;
 	}
 
 	@Override
 	public boolean destroy(String username, int id) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean deleted = false;
+		Optional<EventComment> ePOpt = eCRepo.findById(id);
+		User user = uRepo.findByUsername(username);
+		EventComment eComm = null;
+		
+		if (user != null) {
+			if (ePOpt.isPresent()) {
+				eComm = ePOpt.get();
+				eComm.setEnabled(deleted);
+				uRepo.save(user);
+			deleted = true;
+			return deleted;
+		}
+		}
+		return deleted;
 	}
+
 
 }
