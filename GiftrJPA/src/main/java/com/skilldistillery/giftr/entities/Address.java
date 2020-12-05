4fd8d08aa31ce759192fbda43b1f64a7144b893c
Jess Tucker <tuckerjess92@gmail.com>
2020-12-04 18:00:33 -0600

    Address entity added

---
+++ GiftrJPA/src/main/java/com/skilldistillery/giftr/entities/Address.java
@@ -0,0 +1,120 @@
+package com.skilldistillery.giftr.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String street;
+	private String street2;
+	private String country;
+	private String zip;
+	
+	@Column(name = "state_province")
+	private String stateProvince;
+	
+	private int enabled;
+
+	public Address() {
+		super();
+	}
+
+	public Address(int id) {
+		super();
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet1() {
+		return street;
+	}
+
+	public void setStreet1(String street1) {
+		this.street = street1;
+	}
+
+	public String getStreet2() {
+		return street2;
+	}
+
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getStateProvince() {
+		return stateProvince;
+	}
+
+	public void setStateProvince(String stateProvince) {
+		this.stateProvince = stateProvince;
+	}
+
+	public int getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressTest [id=" + id + ", street1=" + street + ", street2=" + street2 + ", country=" + country
+				+ ", zip=" + zip + ", stateProvince=" + stateProvince + ", enabled=" + enabled + "]";
+	}
+	
+	
+	
+}
---
+++ GiftrJPA/src/test/java/com/skilldistillery/giftr/entities/AddressTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.giftr.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AddressTest {
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("giftrPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		address = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(address);
+		assertEquals("11111", address.getZip());
+
+	}
+}
