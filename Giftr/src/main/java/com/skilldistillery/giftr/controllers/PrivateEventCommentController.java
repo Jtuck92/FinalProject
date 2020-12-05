1a82e1b6366da9bf2508fecc901211b156736fa5
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-12-05 14:43:48 -0700

    Private Comment repo/service/serviceImpl created and tested

---
+++ Giftr/src/main/java/com/skilldistillery/giftr/controllers/PrivateEventCommentController.java
@@ -0,0 +1,104 @@
+package com.skilldistillery.giftr.controllers;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.giftr.entities.EventPost;
+import com.skilldistillery.giftr.entities.PrivateComment;
+import com.skilldistillery.giftr.services.EventPostService;
+import com.skilldistillery.giftr.services.PrivateCommentService;
+
+@CrossOrigin({ "*", "http://localhost:4210" })
+@RestController
+@RequestMapping("api")
+public class PrivateEventCommentController {
+	
+
+	@Autowired
+	private PrivateCommentService pCommentSrv;
+
+	private String username = "22";
+
+	@GetMapping("privateComments")
+	public Set<PrivateComment> index() {
+		return pCommentSrv.index(username);
+	}
+
+	@GetMapping("privateComments/{id}")
+	public PrivateComment show(@PathVariable int id, HttpServletResponse response) {
+
+		try {
+			PrivateComment pComment = pCommentSrv.show(username, id);
+			response.setStatus(200);
+			if(pComment == null ) {
+				response.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(404);
+		}
+		return pCommentSrv.show(username, id);
+	}
+
+	@PostMapping("privateComments")
+	public PrivateComment create(@RequestBody PrivateComment pComment, HttpServletRequest req,
+			HttpServletResponse res) {
+		pComment = pCommentSrv.create(username, pComment);
+		if (pComment == null) {
+			res.setStatus(404);
+		} else {
+			res.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			url.append("/").append(pComment.getId());
+			req.setAttribute("Location", url.toString());
+		}
+		return pCommentSrv.create(username, pComment);
+//			return pCommentSrv.create(principal.getName(), pComment);
+	}
+
+	@PutMapping("privateComments/{id}")
+	public PrivateComment update(@PathVariable int id, @RequestBody PrivateComment pComment, HttpServletRequest req,
+			HttpServletResponse res) {
+		try {
+			pComment = pCommentSrv.update(username, id, pComment);
+			if (pComment == null) {
+				res.setStatus(404);
+				pComment = null;
+			}
+			res.setStatus(201);
+			res.setHeader("Location", "api/eventPosts/" + pComment.getId());
+		} catch (Exception e) {
+			res.setStatus(400);
+		}
+		System.err.println(pComment);
+		return pComment;
+	}
+	
+	@DeleteMapping("privateComments/{id}")
+	public void destroy(HttpServletRequest req, HttpServletResponse res, @PathVariable int id) {
+		try {
+			if (pCommentSrv.destroy(username, id)) {
+				res.setStatus(204);
+			} else {
+				res.setStatus(404);
+			}
+		} catch (Exception e) {
+			res.setStatus(400);
+		}
+	}
+}
--- Giftr/src/main/java/com/skilldistillery/giftr/services/PrivateCommentServiceImpl.java
+++ Giftr/src/main/java/com/skilldistillery/giftr/services/PrivateCommentServiceImpl.java
@@ -1,39 +1,106 @@
 package com.skilldistillery.giftr.services;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
-import com.skilldistillery.giftr.entities.PrivateComment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.skilldistillery.giftr.entities.PrivateComment;
+import com.skilldistillery.giftr.entities.User;
+import com.skilldistillery.giftr.repositories.PrivateCommentRepository;
+import com.skilldistillery.giftr.repositories.UserRepository;
+
+@Service
 public class PrivateCommentServiceImpl implements PrivateCommentService {
+
+	@Autowired
+	private PrivateCommentRepository pEventCommentRepo;
+	@Autowired
+	private UserRepository uRepo;
+
 
 	@Override
 	public Set<PrivateComment> index(String username) {
-		// TODO Auto-generated method stub
+		User user = uRepo.findByUsername(username);
+		if (user != null) {
+			Set<PrivateComment> activePrivateEventComment = new HashSet<PrivateComment>();
+			List<PrivateComment> all = pEventCommentRepo.findAll();
+			for (PrivateComment pEventComment : all) {
+				System.err.println(all.size());
+				activePrivateEventComment.add(pEventComment);
+			}
+			return activePrivateEventComment;
+		}
 		return null;
 	}
 
 	@Override
 	public PrivateComment show(String username, int id) {
-		// TODO Auto-generated method stub
+		Optional<PrivateComment> pCommentOpt = pEventCommentRepo.findById(id);
+		PrivateComment pC = null;
+		if (pCommentOpt.isPresent()) {
+			pC = pCommentOpt.get();
+			if (pC.isEnabled()) {
+				User user = uRepo.findByUsername(username);
+				if (user.getPrvEventComments().contains(pC)) {
+					return pC;
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public PrivateComment create(String username, PrivateComment event) {
-		// TODO Auto-generated method stub
+		User user = uRepo.findByUsername(username);
+		if (user != null) {
+			pEventCommentRepo.saveAndFlush(event);
+			user.getPrvEventComments().add(event);
+			uRepo.save(user);
+			return event;
+		}
 		return null;
 	}
 
 	@Override
 	public PrivateComment update(String username, int id, PrivateComment event) {
-		// TODO Auto-generated method stub
+		User user = uRepo.findByUsername(username);
+		if (user != null) {
+			Optional<PrivateComment> pCommentOpt = pEventCommentRepo.findById(id);
+			PrivateComment pC = null;
+			if (pCommentOpt.isPresent()) {
+				pC = pCommentOpt.get();
+				if (pC.isEnabled()) {
+					if (pC.getComment() != null) {
+						pC.setComment(event.getComment());
+					}
+					pEventCommentRepo.saveAndFlush(pC);
+					uRepo.saveAndFlush(user);
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public boolean destroy(String username, int id) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean deleted = false;
+		Optional<PrivateComment> pCommentOpt = pEventCommentRepo.findById(id);
+		User user = uRepo.findByUsername(username);
+		PrivateComment pC = null;
+		if (user != null) {
+			if (pCommentOpt.isPresent()) {
+				pC = pCommentOpt.get();
+				pC.setEnabled(deleted);
+				uRepo.save(user);
+				deleted = true;
+				return deleted;
+			}
+		}
+		return deleted;
 	}
 
 }
