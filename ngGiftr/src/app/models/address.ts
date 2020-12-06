669c5de0e7bb799b1b38babea29e27ccd9f5fb1a
JustLiving <JustinTLivingston@gmail.com>
2020-12-06 11:17:16 -0800

    Updated Address Model

--- ngGiftr/src/app/models/address.ts
+++ ngGiftr/src/app/models/address.ts
@@ -1,2 +1,28 @@
-export class Address {
+export class  Address {
+id: number;
+street: string;
+street2: string;
+country: string;
+zip: string;
+stateProvince: string;
+enabled: boolean;
+
+  constructor(
+    id?: number,
+    street?: string,
+    street2?: string,
+    country?: string,
+    zip?: string,
+    stateProvince?: string,
+   enabled?: boolean
+  )   {
+    this.id = id;
+    this.street = street;
+    this.street2 = street2;
+    this.country = country;
+    this.zip = zip;
+    this.stateProvince = stateProvince;
+    this.enabled = enabled;
+  }
 }
+
