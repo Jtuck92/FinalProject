fe637bfadeb38f3714530cef013df48b2a4c837d
Jess Tucker <tuckerjess92@gmail.com>
2020-12-06 13:18:49 -0600

    Building models

--- ngGiftr/src/app/models/event.ts
+++ ngGiftr/src/app/models/event.ts
@@ -1,2 +1,39 @@
 export class Event {
+  id: number;
+  name: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  enabled: boolean;
+  budgetId: number;
+  imageUrl: string;
+  creatorId: number;
+  createdDate: string;
+  lastUpdate: string;
+
+  constructor(
+    id?: number,
+    name?: string,
+    description?: string,
+    startDate?: string,
+    endDate?: string,
+    enabled?: boolean,
+    budgetId?: number,
+    imageUrl?: string,
+    creatorId?: number,
+    createdDate?: string,
+    lastUpdate?: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.enabled = enabled;
+    this.budgetId = budgetId;
+    this.imageUrl = imageUrl;
+    this.creatorId = creatorId;
+    this.createdDate = createdDate;
+    this.lastUpdate = lastUpdate;
+  }
 }
--- ngGiftr/src/app/models/gift.ts
+++ ngGiftr/src/app/models/gift.ts
@@ -1,2 +1,39 @@
 export class Gift {
+  id: number;
+  price: number;
+  weight: number;
+  description: string;
+  enabled: boolean;
+  eventId: number;
+  gifterId: number;
+  receiverId: number;
+  rating: number;
+  name: string;
+  imageUrl: string;
+
+  constructor(
+    id?: number,
+    price?: number,
+    weight?: number,
+    description?: string,
+    enabled?: boolean,
+    eventId?: number,
+    gifterId?: number,
+    receiverId?: number,
+    rating?: number,
+    name?: string,
+    imageUrl?: string
+  ) {
+    this.id = id;
+    this.price = price;
+    this.weight = weight;
+    this.description = description;
+    this.enabled = enabled;
+    this.eventId = eventId;
+    this.gifterId = gifterId;
+    this.receiverId = receiverId;
+    this.rating = rating;
+    this.name = name;
+    this.imageUrl = imageUrl;
+  }
 }
