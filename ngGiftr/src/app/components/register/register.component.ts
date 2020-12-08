b6871b35635a9789de1500ed1f85ddc085c7f8b9
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-12-08 11:14:38 -0700

    styles.css Register almost there

--- ngGiftr/src/app/components/register/register.component.ts
+++ ngGiftr/src/app/components/register/register.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
     this.auth.isHomePageComponent(false);
   }
+
+
+
 
 }
