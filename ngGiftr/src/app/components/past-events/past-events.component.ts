2bf9cca10aa4f585f383ed1bea1822894e7fbf8f
JustLiving <JustinTLivingston@gmail.com>
2020-12-08 16:07:25 -0800

    Stubbed out Past events page

--- ngGiftr/src/app/components/past-events/past-events.component.ts
+++ ngGiftr/src/app/components/past-events/past-events.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { EventService } from 'src/app/service/event.service';
+import { PrivateEventService } from 'src/app/service/private-event.service';
 
 @Component({
   selector: 'app-past-events',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PastEventsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventSvc: EventService, private pEventSrv: PrivateEventService, private auth: AuthService, private router: Router) { }
+
+  events: Event[];
+  selected: Event = null;
 
   ngOnInit(): void {
+    this.auth.isHomePageComponent(true);
+    this.loadEvents();
   }
+  loadEvents(): void {
+    this.eventSvc.index().subscribe(
+      (data) => {
+        this.events = data;
+        console.log(this.events);
+      },
+      (err) => {
+        console.error('WorkoutComponent.LoadWorkout(); retrive failed');
 
+      }
+    );
+  }
+  eventResult(event){
+    this.selected = event;
+    localStorage.setItem('event' , "" + this.selected.id);
+    this.router.navigateByUrl("/eventDetails");
+  }
 }
