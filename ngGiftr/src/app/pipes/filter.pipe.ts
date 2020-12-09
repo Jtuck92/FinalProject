79a6392f846d6ec9cfe3e1b9baff9a982dd2cd6d
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-12-09 16:13:23 -0700

    Search Parameter Fix

--- ngGiftr/src/app/pipes/filter.pipe.ts
+++ ngGiftr/src/app/pipes/filter.pipe.ts
@@ -1,3 +1,4 @@
+import { SearchResultsComponent } from './../components/search-results/search-results.component';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,16 +6,33 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(items: any[], field: string, value: string): any[] {
+  transform(items: any[], field: string, field2: string, value: string): any[] {
+    var results = [];
+    var results1 = [];
+    var results2 = [];
     if (!items) {
       return [];
     }
     if (!field || !value) {
       return items;
     }
+results1 = items.filter((singleItem) =>
+singleItem[field].toLowerCase().includes(value.toLowerCase())
+);
+results2 = items.filter((singleItem) =>
+singleItem[field2].toLowerCase().includes(value.toLowerCase())
+);
 
-    return items.filter((singleItem) =>
-      singleItem[field].toLowerCase().includes(value.toLowerCase())
-    );
+results1.forEach(r => {
+  results.push(r);
+});
+
+results2.forEach(r => {
+  results.push(r);
+
+});
+
+
+    return results;
   }
 }
