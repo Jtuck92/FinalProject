770e364ecd76225f46c2008d997a6504234df752
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-12-11 12:11:34 -0700

    All Admin tables added

--- ngGiftr/src/app/components/admin/admin.component.ts
+++ ngGiftr/src/app/components/admin/admin.component.ts
@@ -90,7 +90,7 @@ export class AdminComponent implements OnInit {
     this.loadEvents();
     this.loadGifts();
     this.loadPayments();
-    this.loadPrivateEventComments();
+    // this.loadPrivateEventComments();
     this.loadPrivateEventPosts();
     this.loadPrivateEvents();
     this.loadUsers();
@@ -210,16 +210,16 @@ export class AdminComponent implements OnInit {
     );
   }
   // LOAD ALL Private Event Comments IN DB ====================================
-  loadPrivateEventComments(): void {
-    this.privateCommetSvc.index().subscribe(
-      (data) => {
-        this.privateEventComments = data;
-      },
-      (err) => {
-        console.error('Admin LoadPrivateEventComments(); retrieve failed');
-      }
-    );
-  }
+  // loadPrivateEventComments(): void {
+  //   this.privateCommetSvc.index().subscribe(
+  //     (data) => {
+  //       this.privateEventComments = data;
+  //     },
+  //     (err) => {
+  //       console.error('Admin LoadPrivateEventComments(); retrieve failed');
+  //     }
+  //   );
+  // }
   // LOAD ALL Event types IN DB ====================================
   loadEventTypes(): void {
     this.eTypeSvc.index().subscribe(
