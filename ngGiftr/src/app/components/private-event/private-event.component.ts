3651b397af71c98940a7dab8b88846ed5ddc5347
Jess Tucker <tuckerjess92@gmail.com>
2020-12-12 11:12:31 -0600

    Added create functionality to Private Event component

--- ngGiftr/src/app/components/private-event/private-event.component.ts
+++ ngGiftr/src/app/components/private-event/private-event.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Budget } from 'src/app/models/budget';
 import { PrivateEvent } from 'src/app/models/private-event';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/service/auth.service';
@@ -9,7 +10,7 @@ import { UserService } from 'src/app/service/user.service';
 @Component({
   selector: 'app-private-event',
   templateUrl: './private-event.component.html',
-  styleUrls: ['./private-event.component.css']
+  styleUrls: ['./private-event.component.css'],
 })
 export class PrivateEventComponent implements OnInit {
   stringId: string;
@@ -18,21 +19,27 @@ export class PrivateEventComponent implements OnInit {
   user: any;
   uEPipe: any;
   selectedPrivateEvent: any;
+  errors: any[];
+  newEvent: any;
+  newBudget: any;
+  budgetSvc: any;
+  eventSvc: any;
 
-  constructor(private userSvc: UserService,
+  constructor(
+    private userSvc: UserService,
     private privateEventSvc: PrivateEventService,
     private auth: AuthService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
-    users: User[] = [];
-    selectedUser: User = null;
-    events: Event[] = [];
-    selected: PrivateEvent = new PrivateEvent();
-    selectedEvent: PrivateEvent = null;
-    privateEvent: PrivateEvent[] = [];
+  users: User[] = [];
+  selectedUser: User = null;
+  events: Event[] = [];
+  selected: PrivateEvent = new PrivateEvent();
+  selectedEvent: PrivateEvent = null;
+  privateEvent: PrivateEvent[] = [];
 
-
-    newImageUrl: string = "";
+  newImageUrl: string = '';
 
   ngOnInit(): void {
     this.auth.isHomePageComponent(true);
@@ -42,36 +49,86 @@ export class PrivateEventComponent implements OnInit {
     this.userSvc.index().subscribe(
       (data) => {
         this.users = data;
-        // console.log(this.users);
       },
       (err) => {
         console.error('UserComponent.LoadUser(); retrieve failed');
       }
     );
   }
-  userResult(user){
+  createPrivateEvent() {
+    this.errors = [];
+    if (this.newEvent.name == undefined) {
+      this.errors.push('Giftr name must be filled out');
+    }
+    if (this.newEvent.description == undefined) {
+      this.errors.push('Giftr description must be filled defined');
+    }
+    if (this.newEvent.startDate == undefined) {
+      this.errors.push('Giftr start date must be selected');
+    }
+    if (this.newEvent.endDate == undefined) {
+      this.errors.push('Giftr end date must be selected');
+    }
+    if (this.newBudget == null) {
+      this.errors.push('Giftr budget must be selected');
+    }
+    if (this.newEvent.imageUrl == undefined) {
+      this.errors.push('Giftr image must be provided');
+    }
+    if (this.errors.length == 0) {
+      this.budgetSvc.show(this.newBudget.id).subscribe(
+        (data) => {
+          this.newBudget = data;
+          console.log(this.newBudget);
+        },
+        (err) => {
+          console.error('Admin ShowBudget(); retrieve failed');
+        }
+      );
+
+      this.newEvent.budget = this.newBudget;
+      this.newEvent.eventManager = this.user;
+      console.log(this.newEvent);
+
+      this.eventSvc.create(this.newEvent).subscribe(
+        (data) => {
+          localStorage.removeItem('pageView');
+          localStorage.setItem('pageView', 'Content');
+          localStorage.removeItem('listType');
+          localStorage.setItem('listType', 'Events');
+          this.newBudget = new Budget();
+          this.newEvent = new PrivateEvent();
+          location.reload();
+        },
+        (err) => {
+          console.error('Admin LoadEvent(); retrieve failed');
+        }
+      );
+    }
+  }
+  userResult(user) {
     this.selectedUser = user;
-    localStorage.setItem('user', "" + this.selectedUser.id);
-    this.router.navigateByUrl("/users");
+    localStorage.setItem('user', '' + this.selectedUser.id);
+    this.router.navigateByUrl('/users');
   }
   loadPrivateEvent(): void {
     this.privateEventSvc.index().subscribe(
       (data) => {
-        // console.log(data);
         this.privateEvent = data;
-        // console.log(this.privateEvent);
       },
       (err) => {
-        console.error('PrivateEventComponent.LoadPrivateEvent(); retrieve failed');
+        console.error(
+          'PrivateEventComponent.LoadPrivateEvent(); retrieve failed'
+        );
       }
     );
   }
-  privateEventResult(privateEvent){
+  privateEventResult(privateEvent) {
     this.selectedPrivateEvent = privateEvent;
-    localStorage.setItem('privateEvent', "" + this.selectedPrivateEvent.id);
-    this.router.navigateByUrl("/event/:id");
+    localStorage.setItem('privateEvent', '' + this.selectedPrivateEvent.id);
+    this.router.navigateByUrl('/event/:id');
   }
-  addImageUrl(url: string){
-  // TODO
+  addImageUrl(url: string) {
+    // TODO
   }
 }
